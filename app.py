e62a71b771acfbdb39fcad6942cae70aca8e19b3
RANV33R07 <ranveer072002@gmail.com>
2024-09-07 21:15:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
@@ -49,5 +50,8 @@ def deleteAll():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Create database tables if they don't exist
     port = int(os.environ.get('PORT', 5000))  # Use the PORT environment variable or default to 5000
     app.run(host='0.0.0.0', port=port)
+
